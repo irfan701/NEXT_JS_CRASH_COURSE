c1a8b0b860f11b882b5fe35353149a628ed08a7d
irfan701 <irfanhossain701@gmail.com>
2023-06-03 03:15:38 +0400

    Repo Directory dynamic also Suspense fallback added

--- next_app/app/code/repos/[name]/page.jsx
+++ next_app/app/code/repos/[name]/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Repo from "../../../components/Repo";
 import Link from "next/link";
+import {Suspense} from 'react'
+import RepoDirs from "../../../components/RepoDirs";
 
 const RepoPage = ({params:{name}}) => {
     return (
@@ -8,7 +10,13 @@ const RepoPage = ({params:{name}}) => {
             {/*<h2>REPO</h2>*/}
             {/*<p>{params.name}</p>*/}
             <Link href="code/repos" className='btn btn-warning'>Go Back</Link>
-            <Repo name={name}/>
+            <Suspense fallback={<div>Loading Repo</div>}>
+                <Repo name={name}/>
+            </Suspense>
+
+            <Suspense fallback={<div>Loading Directory...</div>}>
+            <RepoDirs name={name}/>
+            </Suspense>
         </div>
     );
 };
--- next_app/app/components/RepoDirs.jsx
+++ next_app/app/components/RepoDirs.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import {FaCodeBranch, FaEye, FaStar} from "react-icons/fa";
+import Link from "next/link";
 
-const RepoDirs = () => {
+
+async function fetchRepoContent(name) {
+
+    const response=await fetch(`https://api.github.com/repos/irfan701/${name}/contents`)
+    await new Promise((resolve,reject)=>setTimeout(resolve,1000))
+    const contents= await response.json()
+    return contents
+}
+
+const  RepoDirs =async ({name}) => {
+   const contents= await fetchRepoContent(name)
+   const dirs=contents.filter((content)=>content.type==='dir')
     return (
         <div>
-            
+         <h3>Directories</h3>
+            <ul>
+                {dirs.map((dir)=>{
+                  return <li key={dir.path}>
+                        <Link href={`/code/repos/${name}/${dir.path}`}>{dir.path}</Link>
+                    </li>
+                })}
+            </ul>
         </div>
     );
 };
 
 export default RepoDirs;
+
+
+
+
