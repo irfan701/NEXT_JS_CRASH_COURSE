faad4a8579324391fe0b5e64a5257017aeb3e3e9
irfan701 <irfanhossain701@gmail.com>
2023-06-02 15:14:04 +0400

    directory-routing

---
+++ next_app/app/about/team/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TeamPage = () => {
+    return (
+        <div>
+            <h2>Nested Team Page ==</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dicta dolore eligendi excepturi illum, laborum officiis ratione vel? Ab alias animi assumenda at autem consequuntur cupiditate distinctio dolore ea eligendi eos ex facere illo inventore maxime natus obcaecati
+                omnis qui quo rem repellat saepe sequi, sunt tempora totam vel veniam.</p>
+        </div>
+    );
+};
+
+export default TeamPage;
